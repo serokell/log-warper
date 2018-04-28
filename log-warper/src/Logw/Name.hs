c27a9b51ef49e558acc66bbd3ed7b3e5bc8a5027
Dmitry Kovanikov <kovanikov@gmail.com>
2018-04-28 19:06:16 +0300

    Fix compilation on GHC-8.0.2

--- log-warper/src/Logw/Name.hs
+++ log-warper/src/Logw/Name.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE StandaloneDeriving   #-}
 {-# LANGUAGE UndecidableInstances #-}
 
 -- | Contains @newtype@ wrapper 'LoggerName' for list of components for logger name.
@@ -26,7 +27,9 @@ example of correspondence between list of components and string:
 -}
 newtype LoggerName = LoggerName
     { unLoggerName :: Vector Text
-    } deriving (Eq, Ord, Generic, Semigroup, Monoid, Container)
+    } deriving (Eq, Ord, Generic, Semigroup, Monoid)
+
+deriving instance Container LoggerName
 
 instance Hashable LoggerName where
     hashWithSalt = foldl' hashWithSalt
